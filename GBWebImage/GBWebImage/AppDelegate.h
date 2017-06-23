aa4d7f3ed5d064e427055f0398ed4e24da2221f8
Leon-G <leon_g@foxmail.com>
2017-06-23 19:12:42 +0800

    no message

---
+++ GBWebImage/GBWebImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GBWebImage
+//
+//  Created by Leon-G on 2017/6/23.
+//  Copyright © 2017年 Leon-G. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GBWebImage/GBWebImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GBWebImage
+//
+//  Created by Leon-G on 2017/6/23.
+//  Copyright © 2017年 Leon-G. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
